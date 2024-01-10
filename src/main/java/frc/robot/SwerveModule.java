bf67210ee4adbee857b84c5473deb2d709c71678
Jack Pace <jpace2@harding.edu>
2024-01-10 13:53:05 -0600

    Test

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.lib.math.Conversions;
 import frc.lib.util.SwerveModuleConstants;
 
-//Test
 public class SwerveModule {
     public int moduleNumber;
     private Rotation2d angleOffset;
