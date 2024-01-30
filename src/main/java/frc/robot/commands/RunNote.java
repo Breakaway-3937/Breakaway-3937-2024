642738cb93e912e6b2f90259ebe2ad4507167cbc
Jack Pace <jpace2@harding.edu>
2024-01-30 17:28:31 -0600

    basic run note

--- src/main/java/frc/robot/commands/RunNote.java
+++ src/main/java/frc/robot/commands/RunNote.java
@@ -4,17 +4,23 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 public class RunNote extends Command {
   /** Creates a new RunNote. */
-  private Intake s_Intake;
+  private final Intake s_Intake;
+  private final Shooter s_Shooter;
+  private final XboxController xboxController;
 
-  public RunNote(Intake s_Intake) {
+  public RunNote(Intake s_Intake, Shooter s_Shooter, XboxController xboxController) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.s_Intake = s_Intake;
-    addRequirements(s_Intake);
+    this.s_Shooter = s_Shooter;
+    this.xboxController = xboxController;
+    addRequirements(s_Intake, s_Shooter);
   }
 
   // Called when the command is initially scheduled.
@@ -24,11 +30,27 @@ public class RunNote extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    if(s_Intake.getShooterSensor() == true){
+    if(xboxController.getLeftTriggerAxis() <= 0.3 && !xboxController.getRawButton(5) && xboxController.getRightTriggerAxis() <= 0.3){
       s_Intake.stop();
     }
-
+    else if(xboxController.getLeftTriggerAxis() > 0.3){
+      s_Intake.intake();
+    }
+    else if(xboxController.getRawButton(5) && !s_Intake.getShooterSensor()){
+      s_Intake.spit();
+    }
+    else if(s_Intake.getShooterSensor()){
+      s_Intake.stop();
+    }
+    if(xboxController.getRawButton(6)){
+      s_Shooter.runShooter(1000);
+    }
+    else{
+      s_Shooter.stopShooter();
+    }
+    if(s_Shooter.atSpeed() && xboxController.getRightTriggerAxis() > 0.3){
+      s_Intake.intake();
+    }
   }
 
   // Called once the command ends or is interrupted.
