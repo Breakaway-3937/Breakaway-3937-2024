00c3945d96036d657a18ee8ae003d94d60a58c7c
Jack Pace <jpace2@harding.edu>
2024-01-30 16:48:14 -0600

    adding loading motor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -18,7 +18,7 @@ import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
 
-  private final CANSparkMax frontIntakeMotor, backIntakeMotor;
+  private final CANSparkMax frontIntakeMotor, backIntakeMotor, loaderMotor;
   private final AnalogInput intake, staging, shooter;
   private final GenericEntry intakeSensor, stagingSensor, shooterSensor;
 
@@ -26,12 +26,13 @@ public class Intake extends SubsystemBase {
   public Intake() {
     frontIntakeMotor = new CANSparkMax(Constants.Intake.FRONT_INTAKE_MOTOR_ID, MotorType.kBrushless);
     backIntakeMotor = new CANSparkMax(Constants.Intake.BACK_INTAKE_MOTOR_ID, MotorType.kBrushless);
+    loaderMotor = new CANSparkMax(Constants.Intake.LOADER_MOTOR_ID, MotorType.kBrushless);
     configIntakeMotors();
     intake = new AnalogInput(Constants.Intake.INTAKE_SENSOR_ID);
     intake.resetAccumulator();
-    staging = new AnalogInput(Constants.Intake.STAGING_SENSOR_ID);
+    staging = new AnalogInput(Constants.Intake.SHOOTER_SENSOR_ID);
     staging.resetAccumulator();
-    shooter = new AnalogInput(Constants.Intake.SHOOTER_SENSOR_ID);
+    shooter = new AnalogInput(Constants.Intake.BABY_SHOOTER_SENSOR_ID);
     shooter.resetAccumulator();
     intakeSensor = Shuffleboard.getTab("Intake").add("Intake Sensor", 0).withPosition(0, 0).getEntry();
     stagingSensor = Shuffleboard.getTab("Intake").add("Staging Sensor", 0).withPosition(1, 0).getEntry();
@@ -45,22 +46,29 @@ public class Intake extends SubsystemBase {
     backIntakeMotor.restoreFactoryDefaults();
     backIntakeMotor.setIdleMode(IdleMode.kBrake);
 
+    loaderMotor.restoreFactoryDefaults();
+    loaderMotor.setIdleMode(IdleMode.kBrake);
+
     frontIntakeMotor.setSmartCurrentLimit(35);
     backIntakeMotor.setSmartCurrentLimit(35);
+    loaderMotor.setSmartCurrentLimit(35);
 
-    backIntakeMotor.follow(frontIntakeMotor);
+    backIntakeMotor.follow(frontIntakeMotor, true);
   }
 
   public void intake(){
     frontIntakeMotor.set(1);
+    loaderMotor.set(1);
   }
 
   public void spit(){
     frontIntakeMotor.set(-1);
+    loaderMotor.set(-1);
   }
 
   public void stop(){
     frontIntakeMotor.stopMotor();
+    loaderMotor.stopMotor();
   }
 
   @Override
