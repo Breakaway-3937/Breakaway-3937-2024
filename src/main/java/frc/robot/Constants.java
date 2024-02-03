3d5b527719a804918f3be3947d40c6f5536a67cd
Jack Pace <jpace2@harding.edu>
2024-02-02 18:43:56 -0600

    fixing value

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -183,7 +183,7 @@ public final class Constants {
     public static final class Intake {
         public static final int FRONT_INTAKE_MOTOR_ID = 17;
         public static final int BACK_INTAKE_MOTOR_ID = 16;
-        public static final int LOADER_MOTOR_ID = 5;
+        public static final int LOADER_MOTOR_ID = 2;
         public static final int INTAKE_SENSOR_ID = 0; //FIXME
         public static final int SHOOTER_SENSOR_ID = 1; //FIXME
         public static final int BABY_SHOOTER_SENSOR_ID = 2; //FIXME
