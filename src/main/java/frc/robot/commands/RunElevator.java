cfdedc77b052c62e7e2df7c706085da181faafcc
Jack Pace <jpace2@harding.edu>
2024-02-04 22:09:24 -0600

    logic done (for now)

--- src/main/java/frc/robot/commands/RunElevator.java
+++ src/main/java/frc/robot/commands/RunElevator.java
@@ -6,8 +6,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.Shooter;
 
 public class RunElevator extends Command {
   private final Elevator s_Elevator;
@@ -23,9 +24,8 @@ public class RunElevator extends Command {
   private final double elevatorAmp = 0;
   private final double elevatorTrap = 0;
   private final double elevatorSource = 0;
-  private double elevatorNext = 0;
-  private double wristNext = 0;
-  private boolean amp, trap, source, protect;
+  private boolean amp, trap, source, protect, ohCrap, climb;
+  public static boolean deadShooter, trapStage1, trapStage2, startStage1, startStage2, reverse, trapScored, retracting;
 
   /** Creates a new RunElevator. */
   public RunElevator(Elevator s_Elevator, XboxController xboxController) {
@@ -38,67 +38,225 @@ public class RunElevator extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    elevatorNext = elevatorProtect;
-    wristNext = wristProtect;
+    protect = true;
+    amp = false;
+    trap = false;
+    source = false;
+    ohCrap = false;
+    trapStage1 = false;
+    trapStage2 = false;
+    startStage1 = false;
+    startStage2 = false;
+    deadShooter = false;
+    reverse = false;
+    climb = false;
+    trapScored = false;
+    retracting = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //Extend Climb
+    if(xboxController.getPOV() == Constants.Controllers.UP){
+      amp = false;
+      trap = false;
+      source = false;
+      protect = false;
+      ohCrap = false;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = true;
+      reverse = false;
+      climb = true;
+      retracting = false;
+    }
+    //Retract Climb
+    else if(xboxController.getPOV() == Constants.Controllers.DOWN){
+      amp = false;
+      trap = false;
+      source = false;
+      protect = false;
+      ohCrap = false;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = false;
+      reverse = false;
+      climb = false;
+      retracting = true;
+    }
     //Protect
-    if(xboxController.getRawButton(3)){
-      elevatorNext = elevatorProtect;
-      wristNext = wristProtect;
+    else if(xboxController.getRawButton(3)){
       amp = false;
       trap = false;
       source = false;
       protect = true;
+      ohCrap = false;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = false;
+      reverse = false;
+      climb = false;
+      retracting = false;
     }
     //Oh Crap!
     else if(xboxController.getRawButton(2)){
-      elevatorNext = elevatorProtect;
-      wristNext = wristProtect;
       amp = false;
       trap = false;
       source = false;
       protect = false;
+      ohCrap = true;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = true;
+      reverse = false;
+      climb = false;
+      retracting = false;
     }
     //Amp
     else if(xboxController.getRawButton(1)){
-      elevatorNext = elevatorAmp;
-      wristNext = wristAmp;
       amp = true;
       trap = false;
       source = false;
       protect = false;
+      ohCrap = false;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = true;
+      reverse = false;
+      climb = false;
+      retracting = false;
     }
     //Trap
     else if(xboxController.getRawButton(4)){
-      elevatorNext = elevatorTrap;
-      wristNext = wristTrap;
       amp = false;
       trap = true;
       source = false;
       protect = false;
+      ohCrap = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = true;
+      reverse = false;
+      climb = false;
+      retracting = false;
     }
     //Source
     else if(xboxController.getRawButton(7)){
-      elevatorNext = elevatorSource;
-      wristNext = wristSource;
       amp = false;
       trap = false;
       source = true;
       protect = false;
+      ohCrap = false;
+      trapStage1 = false;
+      trapStage2 = false;
+      startStage1 = false;
+      startStage2 = false;
+      deadShooter = true;
+      reverse = false;
+      climb = false;
+      retracting = false;
+      Robot.robotContainer.s_LED.resetColors();
+      Robot.robotContainer.s_LED.orange();
     }
     
-    if(Shooter.isSafe()){
+    if(Robot.robotContainer.s_Shooter.isSafe()){
       if(protect){
-        s_Elevator.setElevator(elevatorNext);
-        s_Elevator.setBabyWrist(wristNext);
+        s_Elevator.setElevator(elevatorProtect);
+        s_Elevator.setBabyWrist(wristProtect);
       }
       else if(trap){
-        s_Elevator.setElevator(elevatorHandoff);
-        s_Elevator.setBabyWrist(wristHandoff);
+        if(!trapStage2 && !Robot.robotContainer.s_Intake.getBabyShooterSensor() && (Robot.robotContainer.s_Intake.getShooterSensor() || Robot.robotContainer.s_Intake.getIntakeSensor())){
+          s_Elevator.setElevator(elevatorHandoff);
+          s_Elevator.setBabyWrist(wristHandoff);
+        }
+        if(!trapStage2 && s_Elevator.isAtPosition() && !Robot.robotContainer.s_Intake.getBabyShooterSensor() && (Robot.robotContainer.s_Intake.getShooterSensor() || Robot.robotContainer.s_Intake.getIntakeSensor())){
+          s_Elevator.runBabyShooterForward();
+          trapStage1 = true;
+        }
+        if(Robot.robotContainer.s_Intake.getBabyShooterSensor() && (!Robot.robotContainer.s_Intake.getShooterSensor() || !Robot.robotContainer.s_Intake.getIntakeSensor())){
+          s_Elevator.stopBabyShooter();
+          trapStage1 = false;
+          trapStage2 = true;
+        }
+        if(trapStage2){
+          s_Elevator.setElevator(elevatorTrap);
+          s_Elevator.setBabyWrist(wristTrap);
+        }
+        if(trapStage2 && s_Elevator.isAtPosition() && Robot.robotContainer.s_Intake.getBabyShooterSensor()){
+          s_Elevator.runBabyShooterForward();
+        }
+        else if(trapStage2 && s_Elevator.isAtPosition() && !Robot.robotContainer.s_Intake.getBabyShooterSensor()){
+          s_Elevator.stopBabyShooter();
+          trapScored = true;
+        }
+      }
+      else if(amp){
+        if(!startStage1 && !startStage2){
+          s_Elevator.setElevator(elevatorHandoff);
+          s_Elevator.setBabyWrist(wristHandoff);
+        }
+        if(xboxController.getRawButton(8) && !startStage2){
+          s_Elevator.runBabyShooterForward();
+          startStage1 = true;
+        }
+        if(Robot.robotContainer.s_Intake.getBabyShooterSensor() && (!Robot.robotContainer.s_Intake.getShooterSensor() || !Robot.robotContainer.s_Intake.getIntakeSensor())){
+          s_Elevator.stopBabyShooter();
+          startStage1 = false;
+          startStage2 = true;
+        }
+        if(startStage2){
+          s_Elevator.setElevator(elevatorAmp);
+          s_Elevator.setBabyWrist(wristAmp);
+          if(xboxController.getRightTriggerAxis() > 0.3){
+            s_Elevator.runBabyShooterForward();
+          }
+          else{
+            s_Elevator.stopBabyShooter();
+          }
+        }
+      }
+      else if(ohCrap){
+        if(Robot.robotContainer.s_Intake.getBabyShooterSensor() && (!Robot.robotContainer.s_Intake.getShooterSensor() || !Robot.robotContainer.s_Intake.getIntakeSensor())){
+          s_Elevator.setElevator(elevatorHandoff);
+          s_Elevator.setBabyWrist(wristHandoff);
+        }
+        if(s_Elevator.isAtPosition() && Robot.robotContainer.s_Intake.getBabyShooterSensor() && (!Robot.robotContainer.s_Intake.getShooterSensor() || !Robot.robotContainer.s_Intake.getIntakeSensor())){
+          reverse = true;
+          s_Elevator.runBabyShooterReverse();
+        }
+        if(!Robot.robotContainer.s_Intake.getBabyShooterSensor() && (Robot.robotContainer.s_Intake.getShooterSensor() || Robot.robotContainer.s_Intake.getIntakeSensor())){
+          reverse = false;
+          s_Elevator.stopBabyShooter();
+          protect = true;
+          deadShooter = false;
+          ohCrap = false;
+        }
+      }
+      else if(source){
+        if(!Robot.robotContainer.s_Intake.botFull()){
+          s_Elevator.setElevator(elevatorSource);
+          s_Elevator.setBabyWrist(wristSource);
+          s_Elevator.runBabyShooterReverse();
+        }
+        if(Robot.robotContainer.s_Intake.getBabyShooterSensor()){
+          s_Elevator.stopBabyShooter();
+          ohCrap = true;
+          source = false;
+        }
+      }
+      else if(climb){
+        s_Elevator.extendClimb();
       }
     }
   }
